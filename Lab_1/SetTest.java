b4594633cf80e831aca396defa7ba56d5d830a7c
Fjonkan <felixnyiri@gmail.com>
2023-11-13 13:20:25 +0100

    oopsie

--- Lab_1/SetTest.java
+++ Lab_1/SetTest.java
@@ -1,6 +1,24 @@
 import org.junit.jupiter.*;
 public class SetTest {
 
+    @Test
+    void testIntsert(){
+        valid = false;
+        Set set = new Set ();
+        insertedSet = set.insert(2);
+        count = 0;
+        for(int s : insertedSet){
+            if(s == 2){
+                count++;
+            }
+            if(count >= 1){
+                valid = true;
+            }
+            
+        }
+        assert(valid);
+    }
+
     /*
      * Manual whitebox testning
      */
